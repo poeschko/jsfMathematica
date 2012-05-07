a267e0bbd910f9a4171f8a311d1a770c3bc7301e
Jan Pöschko <jan@poeschko.com>
2012-05-07 23:47:37 +0200

    cleanup

--- src/com/poeschko/jsfmathematica/Query.java
+++ src/com/poeschko/jsfmathematica/Query.java
@@ -7,7 +7,6 @@ public class Query {
     // http://reference.wolfram.com/mathematica/JLink/tutorial/WritingJavaProgramsThatUseMathematica.html
     // for a list of typical values for different operating systems
     private static String kernelLinkArgs = "-linkmode launch -linkname '\"/Applications/Mathematica.app/Contents/MacOS/MathKernel\" -mathlink'";
-    //private static String linkLibPath = "/usr/lib/java/libJLinkNativeLibrary.jnilib";
     
     private String query;
     private String result;
@@ -18,20 +17,13 @@ public class Query {
     public Query() {
         ml = null;
 
-        //System.out.println("Creating link to: " + kernelPath + " (lib: " + linkLibPath + ")");
         try {
-            //System.setProperty("com.wolfram.jlink.libdir", linkLibPath);
             ml = MathLinkFactory.createKernelLink(kernelLinkArgs);
             ml.discardAnswer(); // get rid of initial InputNamePacket sent after launch
         } catch (MathLinkException e) {
             System.out.println("Fatal error opening link: " + e.getMessage());
         }
     }
-    
-    /*@Override
-    public void finalize() {
-        ml.close();
-    }*/
     
     public String getQuery() {
         return query;
@@ -50,19 +42,7 @@ public class Query {
 
     public String execute() {
         executed = true;
-        //result = "hi";
         result = ml.evaluateToInputForm(query, 0);
-        if (result.startsWith("Error occurred"))
-            result = "Error";
-        /*try {
-            ml.evaluate(query);
-            ml.waitForAnswer();
-            Expr result = ml.getExpr();
-            result.toString();
-            //result = ml.getString();
-        } catch (MathLinkException e) {
-            result = "Error occurred: " + e.getMessage();
-        }*/
         return "executed";
     }
 }
--- test/TestProgram.java
+++ test/TestProgram.java
@@ -10,21 +10,13 @@ import com.wolfram.jlink.MathLinkFactory;
 
 public class TestProgram {
     private static String kernelLinkArgs = "-linkmode launch -linkname '\"/Applications/Mathematica.app/Contents/MacOS/MathKernel\" -mathlink'";
-    //private static String linkLibPath = "/Applications/Mathematica.app/SystemFiles/Links/JLink/SystemFiles/Libraries/MacOSX-x86-64/libJLinkNativeLibrary.jnilib";
-    
-    private static KernelLink ml;
     
     public static void main(String[] argv) {
-        ml = null;
-    
-        //System.out.println("Creating link to: " + kernelPath + " (lib: " + linkLibPath + ")");
         try {
-            //System.setProperty("com.wolfram.jlink.libdir", linkLibPath);
             System.out.println("Creating link");
-            ml = MathLinkFactory.createKernelLink(kernelLinkArgs);
+            KernelLink ml = MathLinkFactory.createKernelLink(kernelLinkArgs);
             System.out.println("Link created");
-            ml.discardAnswer(); // get rid of initial InputNamePacket sent after launch
-            
+            ml.discardAnswer(); // get rid of initial InputNamePacket sent after launch        
             ml.close();
         } catch (MathLinkException e) {
             System.out.println("Fatal error opening link: " + e.getMessage());
